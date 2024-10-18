e09685a3cbfadb1eeac97a913106f2c6b6a74558
Shohel <omorshohel164@gmail.com>
2024-10-18 23:53:20 +0300

    printer app for version control task

---
+++ src/PrinterApp.java
@@ -0,0 +1,32 @@
+public class PrinterApp {
+    public static void main(String[] args) {
+
+        System.out.println("Printer App");
+        
+        
+        System.out.print("This app is made by: ");
+        String creator = "Shohel";
+        System.out.println(creator);
+        
+        
+        double number1 = 12;
+        double number2 = 10;
+        // System.out.println(number1);
+        System.out.println("The value of number1 is " + number1);
+        System.out.println("The value of number2 is " + number2);
+
+        
+        double multiplication;
+        multiplication = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + multiplication);
+
+        double addition = number1 + number2;
+        double subtraction = number1 - number2;
+        double division = number1 / number2;
+
+        System.out.println(number1 + " + " + number2 + " = " + addition);
+        System.out.println(number1 + " - " + number2 + " = " + subtraction);
+        System.out.println(number1 + " / " + number2 + " = " + division);
+
+    }
+}
